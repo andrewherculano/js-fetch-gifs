55a155269d49053cd425d578189c36b44ee44050
andrew herculano <andrewherculano@hotmail.com>
2022-01-21 22:46:24 -0300

    fetch gifs and insert into DOM

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+import { apiKey } from '../modules/api-key.js'
+
+const form = document.querySelector('form')
+const gifsContainer = document.querySelector('.gifs-container')
+
+const fetchGifData = async (value) => {
+   try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=${value}`)
+
+      if (!response.ok) {
+         throw new Error('Não foi possível obter os dados da api')
+      }
+      return response.json()
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+const getGifUrlImage = async (value) => {
+   const response = await fetchGifData(value)
+   const url = response.data[0].images.downsized.url
+
+   return url
+}
+
+const getGifTitle = async (value) => {
+   const response = await fetchGifData(value)
+   const title = response.data[0].title
+
+   return title
+}
+
+form.addEventListener('submit', async event => {
+   event.preventDefault()
+
+   const inputValue = event.target.search.value
+   const gifUrl = await getGifUrlImage(inputValue)
+   const gifTitle = await getGifTitle(inputValue)
+
+   gifsContainer.innerHTML += `
+      <div class="gifs-card">
+         <img src="${gifUrl}">
+         <p>${gifTitle}</p>
+      </div>
+   `
+})
